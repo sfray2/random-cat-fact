07949100d2151a620e414cf59aaca65d12cd0589
sfray2 <shaenabfray@gmail.com>
2022-11-29 18:36:27 -0500

    Revert "initial commit"
    This reverts commit 53191b04b1adafd49fb81a3122df782aeb0408dc.

--- src/App.js
+++ src/App.js
@@ -3,29 +3,22 @@ import Axios from 'axios';
 import {useEffect, useState} from "react";
 
 function App() {
-  const [info, setInfo] = useState(null);
-  const [name, setName] = useState("");
-  
-  const fetchData = () => {
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-      setInfo(res.data)
-    })
+  const [catFact, setCatFact] = useState("")
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
   }
 
+  useEffect(()=> {
+    fetchCatFact();
+  },[]);
 
   return (
     <div className="App">
-      <input 
-        placeholder="Ex. Pedro..."
-        onChange={(event)=> {
-          setName(event.target.value)
-        }} 
-      />
-      <button onClick={fetchData}> Predict Age </button>
-
-      <h1>Name: {info?.name}</h1>
-      <h1>Predicted Age: {info?.age}</h1>
-      <h1>Count: {info?.count}</h1>
+      <button onClick={fetchCatFact}> Generate Cat Fact </button>
+      <p> {catFact} </p>
     </div>
   );
 }
